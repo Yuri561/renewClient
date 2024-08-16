ab66a6f11cb77c76dd174a21dcd771b5c4be58d1
Yuri Pierre-Louis <hazelirvin@gmail.com>
2024-08-16 12:37:05 -0400

    logo resize

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -16,7 +16,7 @@ const Header: React.FC = () => {
     <header className='font-[sans-serif] min-h-[60px] tracking-wide z-50 fixed top-0 left-0 w-full header '>
       <TopNav />
       <div className='flex flex-wrap items-center justify-between py-3 px-4 sm:px-10 bg-[#151d20] lg:gap-y-4 gap-y-6 gap-x-4'>
-        <a href="javascript:void(0)"><img src={Logo} alt="logo" className='w-[80px]' /></a>
+        <a href="javascript:void(0)"><img src={Logo} alt="logo" className='w-10 rounded-full' /></a>
         <NavLinks isMenuOpen={isMenuOpen} toggleMenu={toggleMenu} />
         <div className='flex items-center max-sm:ml-auto'>
           {!isMenuOpen && (
